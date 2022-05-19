182718ff3e55b4784d9f9fb58f24bf8b40c86cbb
DESKTOP-HNFV7LQ\SeongsooKim <kss7547@gmail.com>
2022-05-19 21:53:16 +0900

    gitlab test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainMenu from './components/MainMenu';
 
 function App() {
   return (
@@ -8,12 +9,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.\
+          update <code>src/App.js</code> and save to reload.\
         </p>
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+      <MainMenu></MainMenu>
     </div>
   );
 }
